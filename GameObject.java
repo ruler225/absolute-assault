6d04cf390e96d3fbafbda41a68d83fc0e1ee8e27
Youssef Mikhail <youssef.mikhail@outlook.com>
2017-01-16 09:43:34 -0500

    Updated GameObject
    Removed images on base class, added collsion check that (hopefully)
    works.

--- GameObject.java
+++ GameObject.java
@@ -1,10 +1,10 @@
 import java.awt.*;
+import java.awt.image.*;
 import javax.imageio.*;
 import java.io.*;
 
 //Base class for all objects in the game
 public abstract class GameObject{
-  public Image image;
   public int intX;
   public int intY;
   
@@ -14,42 +14,51 @@ public abstract class GameObject{
   private int intSizeY;
   
   
+  //Set the position of the object
   public void setPosition(int intX, int intY){
     this.intX = intX;
     this.intY = intY;
   }
   
+  public abstract void drawObject(Graphics g);
+  
+  //Returns a boolean of true or false if the object collides with another
+  public boolean checkCollision(GameObject object){
+    int intWidth = object.getWidth();
+    int intHeight = object.getHeight();
+    
+    //Check if the two objects collide
+    if (this.intX >= (object.intX - this.getWidth()) && this.intX <= (object.intX + this.getWidth())){
+      if(this.intY >= (object.intY - this.getHeight()) && this.intY <= (object.intY + this.getHeight())){
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  
+  //Return the width of the object
   public int getWidth(){
     return intSizeX;
   }
   
+  //Return the height of the object
   public int getHeight(){
     return intSizeY;
   }
-
-  //Constructor to construct object with a certain size, and a certain image
-  public GameObject(int intWidth, int intHeight, String strFileName){
+ 
+  //Constructor to construct object with a certain size
+  public GameObject(int intWidth, int intHeight){
     this.intSizeX = intWidth;
     this.intSizeY = intHeight;
-    
-    try{
-      this.image = ImageIO.read(new File(strFileName));
-    }catch(IOException e){
-    }
-    image.getWidth(image);
   }
   
-  //Constructor to construct object with a certain size at an initial position with 
-  public GameObject(int intX, int intY, int intHeight, int intWidth, String strFileName){
+  //Constructor to construct object with a certain size at an initial position
+  public GameObject(int intX, int intY, int intHeight, int intWidth){
     this.intSizeX = intWidth;
     this.intSizeY = intHeight;
     this.intX = intX;
     this.intY = intY;
-    
-    try{
-      this.image = ImageIO.read(new File(strFileName));
-    }catch(IOException e){
-    }
   }
   
   
